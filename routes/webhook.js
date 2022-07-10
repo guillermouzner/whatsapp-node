03b68154825f36bc6e40999dc5a7f98260aa119c
guillermouzner <guillermouz16@gmail.com>
2022-07-10 16:47:51 -0300

    webhook post

--- routes/webhook.js
+++ routes/webhook.js
@@ -31,45 +31,44 @@ router.get("/webhook", (req, res) => {
 router.post("/webhook", (req, res) => {
     // Parse the request body from the POST
     let body = req.body;
-    console.log(body);
-    console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
     // Check the Incoming webhook message
     console.log(JSON.stringify(req.body, null, 2));
 
     // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
-    if (req.body.object) {
-        if (
-            req.body.entry &&
-            req.body.entry[0].changes &&
-            req.body.entry[0].changes[0] &&
-            req.body.entry[0].changes[0].value.messages &&
-            req.body.entry[0].changes[0].value.messages[0]
-        ) {
-            let phone_number_id =
-                req.body.entry[0].changes[0].value.metadata.phone_number_id;
-            let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
-            let msg_body =
-                req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
-            axios({
-                method: "POST", // Required, HTTP method, a string, e.g. POST, GET
-                url:
-                    "https://graph.facebook.com/v12.0/" +
-                    phone_number_id +
-                    "/messages?access_token=" +
-                    token,
-                data: {
-                    messaging_product: "whatsapp",
-                    to: from,
-                    text: { body: "Ack: " + msg_body },
-                },
-                headers: { "Content-Type": "application/json" },
-            });
-        }
-        res.sendStatus(200);
-    } else {
-        // Return a '404 Not Found' if event is not from a WhatsApp API
-        res.sendStatus(404);
-    }
+    // if (req.body.object) {
+    //     if (
+    //         req.body.entry &&
+    //         req.body.entry[0].changes &&
+    //         req.body.entry[0].changes[0] &&
+    //         req.body.entry[0].changes[0].value.messages &&
+    //         req.body.entry[0].changes[0].value.messages[0]
+    //     ) {
+    //         let phone_number_id =
+    //             req.body.entry[0].changes[0].value.metadata.phone_number_id;
+    //         let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+    //         let msg_body =
+    //             req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+    //         axios({
+    //             method: "POST", // Required, HTTP method, a string, e.g. POST, GET
+    //             url:
+    //                 "https://graph.facebook.com/v12.0/" +
+    //                 phone_number_id +
+    //                 "/messages?access_token=" +
+    //                 token,
+    //             data: {
+    //                 messaging_product: "whatsapp",
+    //                 to: from,
+    //                 text: { body: "Ack: " + msg_body },
+    //             },
+    //             headers: { "Content-Type": "application/json" },
+    //         });
+    //     }
+    //     res.sendStatus(200);
+    // } else {
+    //     // Return a '404 Not Found' if event is not from a WhatsApp API
+    //     res.sendStatus(404);
+    // }
 });
 
 export default router;
