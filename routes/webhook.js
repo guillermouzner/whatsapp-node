3c5e646446e0ce75b0ad43ef66ff703c3400d75b
guillermouzner <guillermouz16@gmail.com>
2022-07-12 20:11:14 -0300

    wp cloud api

--- routes/webhook.js
+++ routes/webhook.js
@@ -118,8 +118,8 @@ router.post("/webhook", async (req, res) => {
                     });
                 }
                 if (
-                    theTextMessage !== "Hola" ||
-                    (theTextMessage !== "hola" && !isNaN(theTextMessage))
+                    (theTextMessage !== "Hola" || theTextMessage !== "hola") &&
+                    !isNaN(theTextMessage)
                 ) {
                     await Whatsapp.sendText({
                         message:
