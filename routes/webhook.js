34d74a55d1cfc9af4e83d976e8cb4e6f3418f969
guillermouzner <guillermouz16@gmail.com>
2022-07-13 11:13:27 -0300

    wp cloud

--- routes/webhook.js
+++ routes/webhook.js
@@ -87,20 +87,6 @@ router.post("/webhook", async (req, res) => {
                         });
                     }
                 } else {
-                    // await Whatsapp.sendSimpleButtons({
-                    //     recipientPhone: 543814987351,
-                    //     message: `Hola soy Santi, tu asistente virtual en Santander! 🤖`,
-                    //     listOfButtons: [
-                    //         {
-                    //             title: "Mostrar Opciones",
-                    //             id: "see_categories",
-                    //         },
-                    //         {
-                    //             title: "Hablar con un humano",
-                    //             id: "talk_to_human",
-                    //         },
-                    //     ],
-                    // });
                     await Whatsapp.sendText({
                         message: `Hola soy Santi, tu asistente virtual en Santander! 🤖`,
                         recipientPhone: 543814987351,
@@ -162,94 +148,95 @@ router.post("/webhook", async (req, res) => {
                     }
                 }
             }
-            if (
-                typeOfMsg === "simple_button_message" &&
-                incomingMessage.button_reply.id === "see_categories"
-            ) {
-                listaDeSesiones = [
-                    {
-                        title: "Selecciona una opcion",
-                        rows: [
-                            {
-                                title: "operar activos",
-                                description: "1",
-                                id: "operar_activos",
-                            },
-                            {
-                                title: "Operar Dolar MEP",
-                                description: "2",
-                                id: "Operar_Dolar_MEP",
-                            },
-                            {
-                                title: "Transferir a Banco",
-                                description: "3",
-                                id: "Transferir_a_Banco",
-                            },
-                            {
-                                title: "Convertir Cable-Mep",
-                                description: "4",
-                                id: "Convertir_Cable-Mep",
-                            },
-                            {
-                                title: "Abrir cuenta Cocos",
-                                description: "5",
-                                id: "Abrir_cuenta_Cocos",
-                            },
-                            {
-                                title: "Consultar",
-                                description: "6",
-                                id: "consultar",
-                            },
-                            {
-                                title: "Cerrar cuenta",
-                                description: "7",
-                                id: "cerrar_cuenta",
-                            },
-                        ],
-                    },
-                ];
-                await Whatsapp.sendRadioButtons({
-                    recipientPhone: 543814987351,
-                    headerText: "¿En qué puedo ayudarte? 👇",
-                    bodyText:
-                        "1. Mostrar contacto de Guillermo💣\n2. Comprar/ Vender Dólar MEP\n3. Transferir fondos a mi banco (Nuevo)\n4. Convertir dólar CABLE en dólar MEP\n5. Abrir una cuenta en Santander\n6. Hacer una consulta\n7. Cerrar mi cuenta Santander\n\n📈 Cotización indicativa Dólar MEP (mediante Bonos):\n\nVenta: AR$ 279 / Compra: AR$ 285\n",
-                    //Operar Activos (Compra, Venta, Sucripciones a FCI)
-                    footerText:
-                        "Selecciona una de las opciones para continuar:",
-                    listOfSections: listaDeSesiones,
-                });
-                datos.push({ recipientPhone, listaDeSesiones });
-            }
+            /////
+            // if (
+            //     typeOfMsg === "simple_button_message" &&
+            //     incomingMessage.button_reply.id === "see_categories"
+            // ) {
+            //     listaDeSesiones = [
+            //         {
+            //             title: "Selecciona una opcion",
+            //             rows: [
+            //                 {
+            //                     title: "operar activos",
+            //                     description: "1",
+            //                     id: "operar_activos",
+            //                 },
+            //                 {
+            //                     title: "Operar Dolar MEP",
+            //                     description: "2",
+            //                     id: "Operar_Dolar_MEP",
+            //                 },
+            //                 {
+            //                     title: "Transferir a Banco",
+            //                     description: "3",
+            //                     id: "Transferir_a_Banco",
+            //                 },
+            //                 {
+            //                     title: "Convertir Cable-Mep",
+            //                     description: "4",
+            //                     id: "Convertir_Cable-Mep",
+            //                 },
+            //                 {
+            //                     title: "Abrir cuenta Cocos",
+            //                     description: "5",
+            //                     id: "Abrir_cuenta_Cocos",
+            //                 },
+            //                 {
+            //                     title: "Consultar",
+            //                     description: "6",
+            //                     id: "consultar",
+            //                 },
+            //                 {
+            //                     title: "Cerrar cuenta",
+            //                     description: "7",
+            //                     id: "cerrar_cuenta",
+            //                 },
+            //             ],
+            //         },
+            //     ];
+            //     await Whatsapp.sendRadioButtons({
+            //         recipientPhone: 543814987351,
+            //         headerText: "¿En qué puedo ayudarte? 👇",
+            //         bodyText:
+            //             "1. Mostrar contacto de Guillermo💣\n2. Comprar/ Vender Dólar MEP\n3. Transferir fondos a mi banco (Nuevo)\n4. Convertir dólar CABLE en dólar MEP\n5. Abrir una cuenta en Santander\n6. Hacer una consulta\n7. Cerrar mi cuenta Santander\n\n📈 Cotización indicativa Dólar MEP (mediante Bonos):\n\nVenta: AR$ 279 / Compra: AR$ 285\n",
+            //         //Operar Activos (Compra, Venta, Sucripciones a FCI)
+            //         footerText:
+            //             "Selecciona una de las opciones para continuar:",
+            //         listOfSections: listaDeSesiones,
+            //     });
+            //     datos.push({ recipientPhone, listaDeSesiones });
+            // }
 
-            if (
-                typeOfMsg === "simple_button_message" &&
-                incomingMessage.button_reply.id === "talk_to_human"
-            ) {
-                await Whatsapp.sendContact({
-                    recipientPhone: 543814987351,
-                    contact_profile: {
-                        emails: [
-                            {
-                                email: "guillermouz16@gmail.com",
-                                type: "Gmail",
-                            },
-                        ],
-                        name: {
-                            formatted_name: "Guillermo Uzner",
-                            first_name: "Guillermo",
-                            last_name: "Uzner",
-                        },
+            // if (
+            //     typeOfMsg === "simple_button_message" &&
+            //     incomingMessage.button_reply.id === "talk_to_human"
+            // ) {
+            //     await Whatsapp.sendContact({
+            //         recipientPhone: 543814987351,
+            //         contact_profile: {
+            //             emails: [
+            //                 {
+            //                     email: "guillermouz16@gmail.com",
+            //                     type: "Gmail",
+            //                 },
+            //             ],
+            //             name: {
+            //                 formatted_name: "Guillermo Uzner",
+            //                 first_name: "Guillermo",
+            //                 last_name: "Uzner",
+            //             },
 
-                        phones: [
-                            {
-                                phone: "543814987351",
-                                type: "Cel",
-                                wa_id: "543814987351", // optional
-                            },
-                        ],
-                    },
-                });
-            }
+            //             phones: [
+            //                 {
+            //                     phone: "543814987351",
+            //                     type: "Cel",
+            //                     wa_id: "543814987351", // optional
+            //                 },
+            //             ],
+            //         },
+            //     });
+            // }
 
             let estaElnumero = [];
             datos.forEach((item) => {
