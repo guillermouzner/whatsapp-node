4ab845d6847db28dadcfe7c4959434b09836cc3a
guillermouzner <guillermouz16@gmail.com>
2022-07-25 15:50:50 -0300

    wp api

--- controllers/webhook.controller.js
+++ controllers/webhook.controller.js
@@ -70,9 +70,9 @@ export const sendReceiveMessages = async (req, res) => {
             console.log(datos);
             console.log(createAccount);
             console.log(recipientPhone);
-            if (incomingMessage?.context)
-                console.log("reply_button: " + incomingMessage.list_reply.id);
-            else console.log("texto: " + incomingMessage.text.body);
+            // if (incomingMessage?.context)
+            //     console.log("reply_button: " + incomingMessage.list_reply.id);
+            // else console.log("texto: " + incomingMessage.text.body);
 
             await Whatsapp.markMessageAsRead({
                 message_id: message_id,
