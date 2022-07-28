47a24484575635f652817b0a8f0f046a451519d3
guillermouzner <guillermouz16@gmail.com>
2022-07-28 20:18:48 -0300

    wp api

--- utils/whatsappCloud.js
+++
@@ -1,7 +0,0 @@
-import WhatsappCloudAPI from "whatsappcloudapi_wrapper";
-
-export const Whatsapp = new WhatsappCloudAPI({
-    accessToken: process.env.accessToken,
-    senderPhoneNumberId: process.env.senderPhoneNumberId,
-    WABA_ID: process.env.WABA_ID,
-});
